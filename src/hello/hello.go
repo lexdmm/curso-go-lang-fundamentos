ae177fc1f3711626250edbcddfd6ea7bc7c3c5c3
menes <daniel.menescal@users.noreply.github.com>
2021-04-24 19:35:20 -0300

    endereço de memória

--- src/hello/hello.go
+++ src/hello/hello.go
@@ -1,20 +1,19 @@
-package main // define principal pacote da aplicação.
+package main
 
-import (
-	"fmt"
-	"reflect"
-)
+import "fmt"
 
 func main() {
-	var nome = "Lex"
-	var versao = 1.1
-	var idade = 43
-	teste := 0.99
-
-	fmt.Println("OLá Senhor", nome, "sua idade é", idade)
+	nome := "Douglas"
+	idade := 24
+	versao := 1.1
+	fmt.Println("Olá sr.", nome, "sua idade é", idade)
 	fmt.Println("Este programa está na versão", versao)
-	fmt.Println("O tipo da variável versao é", reflect.TypeOf(versao))
-	fmt.Println("O tipo da variável nome é", reflect.TypeOf(nome))
-	fmt.Println("O tipo da variável idade é", reflect.TypeOf(idade))
-	fmt.Println("A teste variável de nome curto foi inferida sem o var, quando utiliza := está implicito que é uma declaração de variável, o tipo dela é", reflect.TypeOf(teste))
+
+	fmt.Println("1- Iniciar Monitoramento")
+	fmt.Println("2- Exibir Logs")
+	fmt.Println("0- Sair do Programa")
+
+	var comando int
+	fmt.Scan("%d", &comando) // &comercial define o ponteiro que vai receber o dado desta variável, não uso scanf e sim scan porque eu ja sei que é inteiro.
+	fmt.Println("O comando escolhido foi", comando, "Endereço da variável", &comando)
 }

922f2136bb305c1e10dc2ea55c8381b016c9472c
menes <daniel.menescal@users.noreply.github.com>
2021-04-24 19:21:42 -0300

    inferição de variáveis

--- src/hello/hello.go
+++ src/hello/hello.go
@@ -1,11 +1,20 @@
 package main // define principal pacote da aplicação.
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+)
 
 func main() {
-	var nome string = "Lex"
-	var versao float32 = 1.1
+	var nome = "Lex"
+	var versao = 1.1
+	var idade = 43
+	teste := 0.99
 
-	fmt.Println("OLá Senhor", nome)
+	fmt.Println("OLá Senhor", nome, "sua idade é", idade)
 	fmt.Println("Este programa está na versão", versao)
+	fmt.Println("O tipo da variável versao é", reflect.TypeOf(versao))
+	fmt.Println("O tipo da variável nome é", reflect.TypeOf(nome))
+	fmt.Println("O tipo da variável idade é", reflect.TypeOf(idade))
+	fmt.Println("A teste variável de nome curto foi inferida sem o var, quando utiliza := está implicito que é uma declaração de variável, o tipo dela é", reflect.TypeOf(teste))
 }

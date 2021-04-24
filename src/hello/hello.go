7d7b833a27729bcc0b3b8677ef799a6b68c45c9d
menes <daniel.menescal@users.noreply.github.com>
2021-04-24 20:46:24 -0300

    modularizar o programa - functions

--- src/hello/hello.go
+++ src/hello/hello.go
@@ -1,21 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
-	nome := "Lex"
-	idade := 24
-	versao := 1.1
-	fmt.Println("Olá sr.", nome, "sua idade é", idade)
-	fmt.Println("Este programa está na versão", versao)
 
-	fmt.Println("1- Iniciar Monitoramento")
-	fmt.Println("2- Exibir Logs")
-	fmt.Println("0- Sair do Programa")
-
-	var comando int
-	fmt.Scan(&comando) // &comercial define o ponteiro que vai receber o dado desta variável, não uso scanf e sim scan porque eu ja sei que é inteiro.
-	fmt.Println("O comando escolhido foi", comando, "Endereço da variável", &comando)
+	exibeIntroducao()
+	exibeMenu()
+	comando := leComando()
 
 	switch comando {
 	case 1:
@@ -24,7 +18,30 @@ func main() {
 		fmt.Println("Exibindo Logs...")
 	case 0:
 		fmt.Println("Saindo do programa...")
+		os.Exit(0)
 	default:
 		fmt.Println("Não conheço este comando")
+		os.Exit(-1)
 	}
+}
+
+func exibeIntroducao() {
+	nome := "Lex"
+	versao := 1.1
+	fmt.Println("Olá, sr.", nome)
+	fmt.Println("Este programa está na versão", versao)
+}
+
+func exibeMenu() {
+	fmt.Println("1- Iniciar Monitoramento")
+	fmt.Println("2- Exibir Logs")
+	fmt.Println("0- Sair do Programa")
+}
+
+func leComando() int {
+	var comando int
+	fmt.Scan(&comando) // &comercial define o ponteiro que vai receber o dado desta variável, não uso scanf e sim scan porque eu ja sei que é inteiro.
+	fmt.Println("O comando escolhido foi", comando, "Endereço da variável", &comando)
+
+	return comando
 }
